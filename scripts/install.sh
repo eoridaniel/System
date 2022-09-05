5a9c123bb9f0072d2b738f1fc5cbe42c990c56de
Eőri Dániel <eori.dani@gmail.com>
2022-09-05 12:17:40 +0200

    package install

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+
+sudo git clone https://aur.archlinux.org/yay-git.git
+sudo chown -R dani:dani ./yay-git
+cd yay-git
+makepkg -si
+cd ..
+rm -rf yay-git
+yay -S brave, visual-studio-code-bin, spotify, minecraft-launcher, gnome-terminal-transparency, hplip, hplip-plugin
+sudo pacman -Syu discord, cups, avahi, steam
+
+
