6157dc254b1f2ebdbd172a183dc93fb7a4cce121
Eőri Dániel <eori.dani@gmail.com>
2022-09-07 19:17:14 +0200

    fix brave and background issues

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ cd yay-git
 makepkg -si
 cd ..
 sudo sed -zi 's/#\[multilib\]\n#/\[multilib\]\n/' /etc/pacman.conf
-yay -Syu brave visual-studio-code-bin spotify minecraft-launcher gnome-terminal-transparency hplip hplip-plugin discord cups avahi steam nss-mdns gnome-shell-extensions ntfs-3g mysql-workbench
+yay -Syu visual-studio-code-bin spotify minecraft-launcher gnome-terminal-transparency hplip hplip-plugin discord cups avahi steam nss-mdns gnome-shell-extensions ntfs-3g mysql-workbench
 sudo systemctl enable cups.service
 sudo systemctl start cups.service
 sudo systemctl enable avahi-daemon.service
@@ -16,11 +16,13 @@ sudo systemctl restart cups.service
 sudo cp pictures/user.jpg  /var/lib/AccountsService/icons/$USER
 sudo cp pictures/wallpaper.jpg /usr/share/backgrounds/wallpaper.jpg
 gsettings set org.gnome.desktop.background picture-uri file:////usr/share/backgrounds/wallpaper.jpg
+gsettings set org.gnome.desktop.background picture-uri-dark file:////usr/share/backgrounds/wallpaper.jpg
 cp scripts/.bashrc ~/.bashrc
 cp scripts/.project_managment.sh ~/.project_managment.sh
 cp scripts/.prompt_style.sh ~/.prompt_style.sh
 sudo cp styles/gtk.css ~/.config/gtk-3.0/gtk.css
 mkdir ~/.icons
 sudo cp -r cursor/Skyrim ~/.icons/Skyrim
+yay -S brave-bin
 source ~/.bashrc
 rm -fr ../System
