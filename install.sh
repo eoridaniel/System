3c55e9b94cc2e7e8772e3bec3be3ad2a76a02123
Eőri Dániel <eori.dani@gmail.com>
2022-09-20 23:02:42 +0200

    add database, add comments

--- install.sh
+++ install.sh
@@ -1,33 +1,50 @@
 #!/bin/bash
 
+#install yay
 sudo git clone https://aur.archlinux.org/yay-git.git
 sudo chown -R $USER:$USER ./yay-git
 cd yay-git
 makepkg -si
 cd ..
-yay -Syu brave-bin
+#install packages
+yay -Syu brave-bin gnome-shell-extensions
 sudo sed -zi 's/#\[multilib\]\n#/\[multilib\]\n/' /etc/pacman.conf
-yay -Syu visual-studio-code-bin spotify minecraft-launcher gnome-terminal-transparency hplip hplip-plugin discord cups avahi steam nss-mdns gnome-shell-extensions ntfs-3g mariadb
+yay -Syu visual-studio-code-bin spotify minecraft-launcher gnome-terminal-transparency hplip hplip-plugin discord cups avahi steam nss-mdns ntfs-3g mariadb
+#install database
+mariadb-install-db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
+sudo systemctl enable mariadb.service
+sudo systemctl start mariadb.service
+sudo sed -zi 's/[mysqld]/[mysqld]\nbind-address = localhost\n/' /etc/my.conf.d/server.conf
+sudo systemctl restart mariadb.service
+#install printer
 sudo systemctl enable cups.service
 sudo systemctl start cups.service
 sudo systemctl enable avahi-daemon.service
 sudo systemctl start avahi-daemon.service
 sudo sed -i 's/hosts: mymachines resolve/hosts: mymachines mdns_minimal \[NOTFOUND=return\] resolve /' /etc/nsswitch.conf
 sudo systemctl restart cups.service
+#set user icon
 sudo cp pictures/user.jpg  /var/lib/AccountsServices/icons/$USER
 sudo sed -i "s/Icon=.*/Icon=\/var\/lib\/AccountsServices\/icons\/$USER/" /var/lib/AccountsServices/users/$USER
+#set wallpaper
 sudo cp pictures/wallpaper.jpg /usr/share/backgrounds/wallpaper.jpg
 gsettings set org.gnome.desktop.background picture-uri file:////usr/share/backgrounds/wallpaper.jpg
 gsettings set org.gnome.desktop.background picture-uri-dark file:////usr/share/backgrounds/wallpaper.jpg
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+#configuer git
 git config --global user.name "Eőri Dániel"
 git config --global user.email "eori.dani@gamil.com"
+#make projects dir
 mkdir ~/Projects
+#install custom bash commands
 cp scripts/.bashrc ~/.bashrc
 cp scripts/.project_managment.sh ~/.project_managment.sh
 cp scripts/.prompt_style.sh ~/.prompt_style.sh
+source ~/.bashrc
+#install gtk style
 sudo cp styles/gtk.css ~/.config/gtk-3.0/gtk.css
+#prepare coursor
 mkdir ~/.icons
 sudo cp -r cursor/Skyrim ~/.icons/Skyrim
-source ~/.bashrc
+#clean up
 rm -fr ../System
